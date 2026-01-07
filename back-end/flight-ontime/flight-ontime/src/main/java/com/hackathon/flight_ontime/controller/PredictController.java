b43ee17ce038beed0d3029c6605343d60c69bff2
Equipo-36 <equipo36@example.com>
2026-01-06 18:52:59 -0600

    Crea rama limpia: añade PredictController, DTOs (DataRequest/DataResponse) y tests

--- back-end/flight-ontime/flight-ontime/src/main/java/com/hackathon/flight_ontime/controller/PredictController.java
+++ back-end/flight-ontime/flight-ontime/src/main/java/com/hackathon/flight_ontime/controller/PredictController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hackathon.flight_ontime.dto.DataRequest;
-import com.hackathon.flight_ontime.dto.DataResponse;
+import com.hackathon.flight_ontime.DTO.DataRequest;
+import com.hackathon.flight_ontime.DTO.DataResponse;
 
 @RestController
 public class PredictController {
